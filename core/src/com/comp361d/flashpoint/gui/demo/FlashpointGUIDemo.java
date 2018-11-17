0deb29ed6f179d5880b01ce53c3ec893ed3ba461
Simon Zheng <simz97@outlook.com>
2018-11-17 18:59:48 -0500

    Add background to login page prototype

--- core/src/com/comp361d/flashpoint/gui/demo/FlashpointGUIDemo.java
+++ core/src/com/comp361d/flashpoint/gui/demo/FlashpointGUIDemo.java
@@ -3,8 +3,10 @@ package com.comp361d.flashpoint.gui.demo;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.Actor;
@@ -15,112 +17,150 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class FlashpointGUIDemo extends ApplicationAdapter {
-	final static String FLASHPOINT = "Flashpoint";
+    static final String FLASHPOINT = "Flashpoint";
 
-	SpriteBatch batch;
-	//Texture txtr;
-	FreeTypeFontGenerator generator;
-	FreeTypeFontGenerator.FreeTypeFontParameter parameter;
-	BitmapFont fontCaptureIt;
-	GlyphLayout gl;
-	Skin skinUI;
-	TextField fdUname;
-	TextField fdPwd;
-	TextButton btnLogin;
-	Stage stage;
-	//Image img;
-	//Table tbl;
-	//Stack stack;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
+    SpriteBatch batch;
 
-		//fontCaptureIt = new BitmapFont(Gdx.files.internal("core/assets/data/default.fnt"));
-		//fontCaptureIt.setColor(Color.RED);
-		//fontCaptureIt.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
-		//fontCaptureIt.getData().setScale(3);
-		generator = new FreeTypeFontGenerator(Gdx.files.internal("core/assets/data/Capture_it.ttf"));
-		parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-		parameter.size = 72;
-		fontCaptureIt = generator.generateFont(parameter);
-		fontCaptureIt.setColor(Color.SCARLET);
-		gl = new GlyphLayout(fontCaptureIt, FLASHPOINT);
+    Skin skinUI;
 
-		skinUI = new Skin(Gdx.files.internal("core/assets/data/uiskin.json"));
+    Label debugLbl;
 
-		btnLogin = new TextButton("Login", skinUI, "default");
-		btnLogin.setWidth(100);
-		btnLogin.setHeight(25);
-		btnLogin.setPosition((Gdx.graphics.getWidth() - btnLogin.getWidth()) / 2, Gdx.graphics.getHeight() / 3 - (btnLogin.getHeight() / 2));
-		btnLogin.addListener(new ClickListener() {
-			@Override
-			public void clicked(InputEvent event, float x, float y) {
-				if (btnLogin.getText().toString().equals("Send")) btnLogin.setText("Sent");
-				else btnLogin.setText("Send");
-			}
-			@Override
-			public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
-				btnLogin.setText("Sent");
-			}
-			@Override
-			public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
-				btnLogin.setText("Send");
-			}
-		});
+    Texture txtrBG;
+    Sprite spriteBG;
 
-		fdUname = new TextField("Username", skinUI, "default");
-		fdUname.setWidth(200);
-		fdUname.setHeight(25);
-		fdUname.setPosition((Gdx.graphics.getWidth() - fdUname.getWidth()) / 2, Gdx.graphics.getHeight() * 3 / 5 - (fdUname.getHeight() / 2));
+    FreeTypeFontGenerator generator;
+    FreeTypeFontGenerator.FreeTypeFontParameter parameter;
+    BitmapFont fontCaptureIt;
+    GlyphLayout gl;
 
-		fdPwd = new TextField("Password", skinUI, "default");
-		fdPwd.setWidth(200);
-		fdPwd.setHeight(25);
-		fdPwd.setPosition((Gdx.graphics.getWidth() - fdUname.getWidth()) / 2, Gdx.graphics.getHeight() / 2 - (fdUname.getHeight() / 2));
-		fdPwd.setPasswordMode(true);
-		fdPwd.setPasswordCharacter('¬');
+    TextField fdUname;
+    TextField fdPwd;
+    TextButton btnLogin;
 
-		stage = new Stage();
-		stage.addActor(fdUname);
-		stage.addActor(fdPwd);
-		stage.addActor(btnLogin);
-		//stage.addActor(fontCaptureIt);
+    Stage stage;
 
-		Gdx.input.setInputProcessor(stage);
-	}
+    // Image img;
+    // Table tbl;
+    // Stack stack;
 
-	@Override
-	public void dispose () {
-		batch.dispose();
-		fontCaptureIt.dispose();
-		generator.dispose();
-		skinUI.dispose();
-		stage.dispose();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		stage.draw();
-		batch.end();
+        skinUI = new Skin(Gdx.files.internal("core/assets/data/uiskin.json"));
 
-		batch.begin();
-		fontCaptureIt.draw(batch, FLASHPOINT, (Gdx.graphics.getWidth() - gl.width) / 2, Gdx.graphics.getHeight() * 9 / 10 - (gl.height / 2));
-		batch.end();
-	}
+        debugLbl = new Label("", skinUI);
+        debugLbl.setPosition(10, 10);
+        debugLbl.setColor(Color.PURPLE);
+        debugLbl.setText(Gdx.graphics.getWidth() + " " + Gdx.graphics.getHeight());
 
-	@Override
-	public void resize(int width, int height) {
-	}
+        txtrBG = new Texture("core/assets/fire_rescue.png");
+        spriteBG = new Sprite(txtrBG);
+        spriteBG.setScale(0.6f);
+        spriteBG.setPosition(-(Gdx.graphics.getWidth() / 2f) - 125, -(Gdx.graphics.getHeight() / 2f) + 30);
 
-	@Override
-	public void pause() {
-	}
+        generator =
+                new FreeTypeFontGenerator(Gdx.files.internal("core/assets/data/Capture_it.ttf"));
+        parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 72;
+        fontCaptureIt = generator.generateFont(parameter);
+        fontCaptureIt.setColor(Color.CORAL);
+        gl = new GlyphLayout(fontCaptureIt, FLASHPOINT);
 
-	@Override
-	public void resume() {
-	}
+        btnLogin = new TextButton("Login", skinUI, "default");
+        btnLogin.setWidth(100);
+        btnLogin.setHeight(25);
+        btnLogin.setPosition(
+                (Gdx.graphics.getWidth() - btnLogin.getWidth()) / 2,
+                Gdx.graphics.getHeight() / 3f - (btnLogin.getHeight() / 2));
+        btnLogin.addListener(
+                new ClickListener() {
+                    @Override
+                    public void clicked(InputEvent event, float x, float y) {
+                        if (btnLogin.getText().toString().equals("Login")) btnLogin.setText("Logging in...");
+                        else btnLogin.setText("Login");
+                    }
+
+                    @Override
+                    public void enter(
+                            InputEvent event, float x, float y, int pointer, Actor fromActor) {
+                        btnLogin.setText("Sent");
+                    }
+
+                    @Override
+                    public void exit(
+                            InputEvent event, float x, float y, int pointer, Actor toActor) {
+                        btnLogin.setText("Send");
+                    }
+                });
+
+        fdUname = new TextField("", skinUI, "default");
+        fdUname.setMessageText("Username");
+        fdUname.setWidth(200);
+        fdUname.setHeight(25);
+        fdUname.setPosition(
+                (Gdx.graphics.getWidth() - fdUname.getWidth()) / 2,
+                Gdx.graphics.getHeight() * 3 / 5f - (fdUname.getHeight() / 2));
+
+        fdPwd = new TextField("", skinUI, "default");
+        fdPwd.setPasswordMode(true);
+        fdPwd.setPasswordCharacter('*');
+        fdPwd.setMessageText("Password");
+        fdPwd.setWidth(200);
+        fdPwd.setHeight(25);
+        fdPwd.setPosition(
+                (Gdx.graphics.getWidth() - fdUname.getWidth()) / 2,
+                Gdx.graphics.getHeight() / 2f - (fdUname.getHeight() / 2));
+
+        stage = new Stage();
+        stage.addActor(fdUname);
+        stage.addActor(fdPwd);
+        stage.addActor(btnLogin);
+        // stage.addActor(fontCaptureIt);
+        stage.addActor(debugLbl);
+
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        fontCaptureIt.dispose();
+        generator.dispose();
+        skinUI.dispose();
+        stage.dispose();
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        batch.disableBlending();
+        spriteBG.draw(batch);
+        batch.enableBlending();
+        batch.end();
+
+        batch.begin();
+        stage.draw();
+        batch.end();
+
+        batch.begin();
+        fontCaptureIt.draw(
+                batch,
+                FLASHPOINT,
+                (Gdx.graphics.getWidth() - gl.width) / 2,
+                Gdx.graphics.getHeight() * 9 / 10f - (gl.height / 2));
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {}
+
+    @Override
+    public void pause() {}
+
+    @Override
+    public void resume() {}
 }
