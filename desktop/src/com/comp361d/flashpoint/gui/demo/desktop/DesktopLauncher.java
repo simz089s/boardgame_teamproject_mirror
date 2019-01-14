7240029a162e62cfde60f19ff420c219b531caaf
jz360 <jacqueshzhang97@gmail.com>
2019-01-14 18:53:47 -0500

    pushing all files

---
+++ desktop/src/com/comp361d/flashpoint/gui/demo/desktop/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package com.comp361d.flashpoint.gui.demo.desktop;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.comp361d.flashpoint.gui.demo.GameScreens.FlashPointGame;
+
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new FlashPointGame(), config);
+
+        // change window size
+        config.width = 1224;
+        config.height = 700;
+    }
+}
