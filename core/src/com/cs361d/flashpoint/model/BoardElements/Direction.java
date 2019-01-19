47cfb3c436a2b5f3e52e1569e335f40d050c7ba0
Elvric <elvric.trombert@mail.mcgill.ca>
2019-01-19 17:44:05 -0500

    Added the new classes

---
+++ core/src/com/cs361d/flashpoint/model/BoardElements/Direction.java
@@ -0,0 +1,10 @@
+package com.cs361d.flashpoint.model.BoardElements;
+
+public enum Direction {
+    TOP("top"),
+    BOTTOM("bottom"),
+    LEFT("left"),
+    RIGHT("right");
+
+    Direction(String text){}
+}
