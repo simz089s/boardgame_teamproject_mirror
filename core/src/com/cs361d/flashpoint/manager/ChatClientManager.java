c3d44a69dca15b7063f25e52988f166204739d03
Simon Zheng <simz97@outlook.com>
2019-02-05 23:50:56 -0500

    Rewrite networking draft with Java.net

--- core/src/com/cs361d/flashpoint/manager/ChatClientManager.java
+++ core/src/com/cs361d/flashpoint/manager/ChatClientManager.java
@@ -1,44 +1,97 @@
 package com.cs361d.flashpoint.manager;
 
-import com.esotericsoftware.kryonet.Connection;
-import com.esotericsoftware.kryonet.Listener;
-import com.esotericsoftware.kryonet.Server;
-import com.esotericsoftware.minlog.Log;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.*;
 
-public class ChatClientManager {
+public class ChatClientManager implements Observer {
+
+    private int tcpPort;
+    private int udpPort;
+
+    private List<String> messages = new LinkedList<String>();
+
+    public ChatClientManager() throws IOException {
+        this("localhost", NetworkManager.PORT_TCP);
+    }
+
+    public ChatClientManager(String host, int tcpPort) throws IOException {
+        this.tcpPort = tcpPort;
+
+        final Socket socket = new Socket(host, tcpPort);
+
+        // Writer in new thread to separate writing message and showing messages. Other than
+        // concurrency
+        // problem in
+        // fitting both in one terminal screen, should not be a problem when using two separate
+        // "boxes"
+        // in GUI (change
+        // System.in etc.) or add locks (TODO)
+        new Thread("Writer") {
+            public void run() {
+                //                try (Scanner sc = new Scanner(System.in);
+                //                     PrintStream ps = new PrintStream(socket.getOutputStream())) {
+                Scanner sc = new Scanner(System.in);
+                try {
+                    PrintStream ps = new PrintStream(socket.getOutputStream());
+                    try {
+                        System.out.print("Client> ");
+                        do {
+                            // Get user input
+                            String msg = sc.nextLine();
+                            if (msg.equals("exit")) break;
+
+                            // Send over socket
+                            ps.println(msg);
+
+                            System.out.print("Client> ");
+                        } while (sc.hasNextLine());
+                    } finally {
+                        ps.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    sc.close();
+                }
+            }
+        }.start();
+
+        Scanner socketInputStreamScanner = new Scanner(socket.getInputStream());
+        try {
+            do {
+                // Get response
+                String response = socketInputStreamScanner.nextLine();
+                System.out.println("Server> " + response);
+                if (response.equals("exit")) break;
+            } while (socketInputStreamScanner.hasNextLine());
+        } finally {
+            socketInputStreamScanner.close();
+        }
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        System.out.println("Server> " + arg);
+    }
+
     public static void main(String[] args) {
+        //        new Thread("Server") {
+        //            public void run() {
+        //                try {
+        //                    ChatServerManager srv = new ChatServerManager();
+        //                } catch (IOException ex) {
+        //                    ex.printStackTrace();
+        //                    System.exit(1);
+        //                }
+        //            }
+        //        }.start();
 
-    ChatServerManager chatMan = new ChatServerManager();
-    Server srv = chatMan.server;
-
-    /*
-     * TODO:
-     * (remove this) test exchange
-     */
-    srv.addListener(
-        new Listener() {
-          public void received(Connection connection, Object object) {
-            if (object instanceof NetworkManager.ChatMessage) {
-              NetworkManager.ChatMessage request = (NetworkManager.ChatMessage) object;
-              System.out.println("In client: " + request.text);
-
-              NetworkManager.ChatMessage response = new NetworkManager.ChatMessage();
-              response.text = "From client: Thanks";
-              connection.sendTCP(response);
-            }
-            if (object instanceof String) {
-              String request = (String) object;
-              System.out.println("In client: " + request);
-
-              String response = "From client: Thanks";
-              connection.sendTCP(response);
-            }
-          }
-        });
-
-    chatMan.connectClient(5000, "localhost", NetworkManager.PORT_TCP, NetworkManager.PORT_UDP);
-
-//        Log.set(Log.LEVEL_DEBUG);
-//        new ChatClientManager();
+        try {
+            ChatClientManager cli = new ChatClientManager();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
