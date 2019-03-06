7a80b93b6554376d1ccb5bbe6bb7d0f9d518e9ae
dch133 <daniel.ch.13@gmail.com>
2019-03-06 12:48:13 -0500

    added imports

--- desktop/src/com/cs361d/flashpoint/desktop/DesktopLauncher.java
+++ desktop/src/com/cs361d/flashpoint/desktop/DesktopLauncher.java
@@ -2,7 +2,9 @@ package com.cs361d.flashpoint.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cs361d.flashpoint.Networking.Client;
 import com.cs361d.flashpoint.Networking.NetworkManager;
+import com.cs361d.flashpoint.Networking.Server;
 import com.cs361d.flashpoint.view.FlashPointGame;
 
 public class DesktopLauncher {
