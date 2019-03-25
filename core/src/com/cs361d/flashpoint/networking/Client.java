fbb107ee286431987e4cbe859b3ed5ba83ad3318
mathieuvachon2 <vmat96@gmail.com>
2019-03-25 17:31:13 -0400

    All client commands now in client (not in networkManager

--- core/src/com/cs361d/flashpoint/networking/Client.java
+++ core/src/com/cs361d/flashpoint/networking/Client.java
@@ -1,6 +1,15 @@
 package com.cs361d.flashpoint.networking;
 
+import com.badlogic.gdx.Gdx;
+import com.cs361d.flashpoint.manager.CreateNewGameManager;
+import com.cs361d.flashpoint.manager.User;
+import com.cs361d.flashpoint.model.BoardElements.FireFighterColor;
+import com.cs361d.flashpoint.screen.BoardChatFragment;
+import com.cs361d.flashpoint.screen.BoardScreen;
 import com.cs361d.flashpoint.screen.FlashPointGame;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -31,21 +40,19 @@ public class Client {
             dout = new DataOutputStream(s.getOutputStream());
 
             // obtain client IP address
-            clientIP = s.getInetAddress().toString().replace("/","");
+            clientIP = s.getInetAddress().toString().replace("/", "");
 
             // readMessage thread constantly listening
             Thread readMessage =
-                    new Thread(new Runnable()
-                    {
+                    new Thread(new Runnable() {
                         @Override
-                        public void run()
-                        {
+                        public void run() {
                             while (notStopped) {
                                 String msg;
                                 try {
                                     // read the message sent to this client
                                     msg = din.readUTF();
-                                    NetworkManager.clientExecuteCommand(msg);
+                                    clientExecuteCommand(msg);
                                 } catch (Exception connectionLost) {
                                     try {
                                         System.out.println("Server disconnected: closing client...");
@@ -63,7 +70,7 @@ public class Client {
                                         System.out.println("Unable to close Streams for Client with IP: " + clientIP);
                                         e.printStackTrace();
                                     }
-                                    }
+                                }
                             }
                         }
                     });
@@ -76,12 +83,22 @@ public class Client {
 
     }
 
-    public void stopClientReadFromClientHandlerThread(){notStopped = false;}
+    public void stopClientReadFromClientHandlerThread() {
+        notStopped = false;
+    }
 
     /* Getters */
-    public String getClientIP() { return clientIP; }
-    public DataInputStream getDin() { return din; }
-    public DataOutputStream getDout() { return dout; }
+    public String getClientIP() {
+        return clientIP;
+    }
+
+    public DataInputStream getDin() {
+        return din;
+    }
+
+    public DataOutputStream getDout() {
+        return dout;
+    }
 
 
     /* Send a message to a server */
@@ -90,8 +107,108 @@ public class Client {
             // write on the output stream
             dout.writeUTF(msg);
             dout.flush();
-        } catch (IOException e) { e.printStackTrace(); }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void stopClientReadingThread() { notStopped = false; }
+    public void stopClientReadingThread() {
+        notStopped = false;
+    }
+
+
+    // Now in client
+    public static void clientExecuteCommand(String msg) {
+        try {
+            JSONParser parser = new JSONParser();
+
+            JSONObject jsonObject = (JSONObject) parser.parse(msg);
+            ClientCommands c = ClientCommands.fromString(jsonObject.get("command").toString());
+            final String message = jsonObject.get("message").toString();
+            String ip = jsonObject.get("IP").toString();
+            System.out.println(message);
+            switch (c) {
+//        case CHATGAME:
+//          if (!message.equals("")) BoardChatFragment.addMessageToGui(message);
+//          break;
+
+                case GAMESTATE:
+                    // Transfer the redraw call to the main thread (that has openGL and GDX)
+                    CreateNewGameManager.loadGameFromString(message);
+                    Gdx.app.postRunnable(
+                            new Runnable() {
+                                @Override
+                                public void run() {
+                                    BoardScreen.redrawBoard();
+                                }
+                            });
+                    break;
+
+                case ASSIGN_FIREFIGHTER:
+                    User.getInstance().assignFireFighter(FireFighterColor.fromString(message));
+                    break;
+
+                case EXITGAME:
+                    Gdx.app.postRunnable(
+                            new Runnable() {
+                                @Override
+                                public void run() {
+                                    BoardScreen.setLobbyPage();
+                                }
+                            });
+                    break;
+
+                case SETBOARDSCREEN:
+                    Gdx.app.postRunnable(
+                            new Runnable() {
+                                @Override
+                                public void run() {
+                                    BoardScreen.setBoardScreen();
+                                }
+                            });
+                    break;
+                case SEND_CHAT_MESSAGES:
+                    Gdx.app.postRunnable(
+                            new Runnable() {
+                                @Override
+                                public void run() {
+//                      BoardScreen.setSideFragment(Fragment.CHAT);
+                                }
+                            });
+                    JSONArray jsa = (JSONArray) parser.parse(message);
+                    for (Object a : jsa) {
+                        final String newMessage = a.toString();
+                        Gdx.app.postRunnable(
+                                new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        final String msg = newMessage;
+                                        BoardChatFragment.addMessageToChat(msg);
+                                    }
+                                });
+                    }
+                    break;
+
+                case ADD_CHAT_MESSAGE:
+                    if (!message.equals("")) {
+                        Gdx.app.postRunnable(
+                                new Runnable() {
+                                    @Override
+                                    public void run() {
+//                      if (BoardScreen.isChatFragment())
+                                        BoardChatFragment.addMessageToChat(message);
+                                    }
+                                });
+                    }
+                    break;
+
+                default:
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
